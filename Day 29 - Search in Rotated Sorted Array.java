c8e9e93d47b1d2d934d20faeaa8e47a1b6031379
Niraj <155253763+Nicode2707@users.noreply.github.com>
2024-12-25 00:31:21 +0530

    Create Day 29 - Search in Rotated Sorted Array.java

---
+++ Day 29 - Search in Rotated Sorted Array.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+
+class Solution {
+    int search(int[] arr, int key) {
+        // Complete this function
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == key){
+                return i;
+            }
+            
+        }
+        return -1;
+    }
+}
